23edd029778169a0ad6cb92a52deaaa592b1a70f
FCassettari <161382704+FCassettari@users.noreply.github.com>
2024-04-17 15:01:27 -0500

    Update dependencies_installation.py

--- dependencies_installation.py
+++ dependencies_installation.py
@@ -1,6 +1,6 @@
 import subprocess
-packages = ["matplotlib", "scikit-learn", "tensorflow", "scikeras"]
+packages = ["matplotlib", "scikit-learn", "tensorflow", "scikeras", "keras"]
 
 for package in packages: 
-      install_pack = f'pip install {package}'
-      os.system(install_pack) 
+    install_pack = f'pip install {package}'
+    os.system(install_pack) 
